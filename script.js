637df8915ca5364524885224e2085cf3632e1947
onyschukoleh <onyschukoleh@gmail.com>
2020-05-22 15:23:46 +0300

    Add code to script.js

--- script.js
+++ script.js
@@ -1,143 +1,126 @@
-// Задание 1
-// Напиши функцию-конструктор Account,
-//  которая создает объект со свойствами login и email.
-//  В prototype функции-конструктора добавь метод getInfo(),
-// который выводит в консоль значения полей login и email объекта который его вызвал.
+// ===============================================================
+/* Задание 1 функциz-конструктор Account, которая создает объект со свойствами login и email.
+  В prototype функции-конструктора добавь метод getInfo(), который выводит в консоль значения полей login и email объекта который его вызвал.*/
+// ===============================================================
+console.log("#============================================");
+function Account(obj) {
+  this.login = obj.login;
+  this.email = obj.email;
+}
 
-// function Account(obj) {
-//   this.login = obj.login;
-//   this.email = obj.email;
-// }
+Account.prototype.getInfo = function () {
+  console.log("#1 : Login: " + this.login + ", " + "Email: " + this.email);
+};
 
-// Account.prototype.getInfo = function () {
-//   console.log("Login: " + this.login + ", " + "Email: " + this.email);
-// };
+const mango1 = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof",
+});
+console.log("#1 :", mango1);
+mango1.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+const poly1 = new Account({
+  login: "Poly",
+  email: "poly@mail.com",
+});
+console.log("#1 :", poly1);
+poly1.getInfo(); // Login: Poly, Email: poly@mail.com
+console.log("#============================================");
+// ===============================================================
+// Задание 2 Kласс User для создания пользователя name - строка, age - число, followers - число
+// метод getInfo() выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+// ===============================================================
+class User {
+  constructor(obj) {
+    (this.name = obj.name),
+      (this.age = obj.age),
+      (this.followers = obj.followers);
+  }
+}
 
-// const mango = new Account({
-//   login: "Mangozedog",
-//   email: "mango@dog.woof",
-// });
-// console.log(mango);
-// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
-// const poly = new Account({
-//   login: "Poly",
-//   email: "poly@mail.com",
-// });
-// console.log(poly);
-// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+const mango2 = new User({
+  name: "Mango",
+  age: 2,
+  followers: 20,
+});
 
-// Задание 2
-// Напиши класс User для создания пользователя со следующим свойствами:
-// name - строка
-// age - число
-// followers - число
-// Добавь метод getInfo(), который, выводит строку: User ${имя} is ${возраст} years old and has ${кол-во фоловеров} followers
+console.log("#2 :", mango2);
+User.prototype.getInfo = function () {
+  console.log(
+    "#2 :",
+    `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
+  );
+};
 
-// class User {
-//   constructor(obj) {
-//     (this.name = obj.name),
-//       (this.age = obj.age),
-//       (this.followers = obj.followers);
-//   }
-// }
-// console.log(User);
+mango2.getInfo(); // User Mango is 2 years old and has 20 followers
 
-// const mango = new User({
-//   name: "Mango",
-//   age: 2,
-//   followers: 20,
-// });
+const poly2 = new User({
+  name: "Poly",
+  age: 3,
+  followers: 17,
+});
+console.log("#2 :", poly2);
+poly2.getInfo(); // User Poly is 3 years old and has 17 followers
+console.log("#============================================");
+// ===============================================================
+// Задание 3 Kласс Storage, который будет создавать объекты для управления складом товаров.
+// При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+// Добавь методы класса:    getItems() - возвращает массив текущих товаров
+//                          addItem(item) - получает новый товар и добавляет его к текущим
+//                          removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+// ===============================================================
+class Storage {
+  constructor(arr) {
+    this.items = arr;
+  }
+}
 
-// console.log(mango);
-// User.prototype.getInfo = function () {
-//   console.log(
-//     `User ${this.name} is ${this.age} years old and has ${this.followers} followers`
-//   );
-// };
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
+console.log("#3 :", storage);
 
-// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+Storage.prototype.getItems = function () {
+  console.log("#3 :", this.items);
+};
+storage.getItems();
 
-// const poly = new User({
-//   name: "Poly",
-//   age: 3,
-//   followers: 17,
-// });
-// console.log(poly);
-// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
 
-// Задание 3
-// Напиши класс Storage, который будет создавать объекты
-// для управления складом товаров.
-// При вызове будет получать один аргумент - начальный массив товаров,
-//  и записывать его в свойство items.
+Storage.prototype.addItem = function (item) {
+  this.items.push(item);
+};
 
-// Добавь методы класса:
+storage.addItem("Дроид");
+console.table("#3 :", storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
 
-// getItems() - возвращает массив текущих товаров
-// addItem(item) - получает новый товар и добавляет его к текущим
-// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
+Storage.prototype.removeItem = function (itemName) {
+  this.items = this.items.filter((item) => item !== itemName);
+};
 
-// class Storage {
-//   constructor(arr) {
-//     this.items = arr;
-//   }
-// }
+storage.removeItem("Пролонгер");
+console.table("#3 :", storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+console.log("#============================================");
 
-// const storage = new Storage([
-//   "Нанитоиды",
-//   "Пролонгер",
-//   "Железные жупи",
-//   "Антигравитатор",
-// ]);
-// console.log(storage);
-
-// Storage.prototype.getItems = function () {
-//   console.log(this.items);
-// };
-// storage.getItems();
-
-// // const items = storage.getItems();
-// // console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
-
-// Storage.prototype.addItem = function (item) {
-//   this.items.push(item);
-// };
-
-// storage.addItem("Дроид");
-// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
-
-// Storage.prototype.removeItem = function (itemName) {
-//   this.items = this.items.filter((item) => item !== itemName);
-// };
-
-// storage.removeItem("Пролонгер");
-// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
-
-// Задание 4
-// Напиши класс StringBuilder. На вход он получает один параметр -
-// строку, которую записывает в свойство _value.
-
-// Добавь классу следующий функционал:
-
-// Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
-// Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
-
+// ===============================================================
+// Задание 4 Класс StringBuilder. На вход он получает один параметр - строку и записывает ее в свойство _value.
+// Добавь классу следующий функционал: Метод prepend(str) - получает парметр str (строку) и добавляет ее в начало value
+//                                      Метод pad(str) - получает парметр str (строку) и добавляет ее в начало и в конец _value
+// ===============================================================
 class StringBuilder {
   constructor(value) {
     this._value = value;
   }
-  // Метод Геттер value - возвращает текущее значение поля _value
   get value() {
     return this._value;
   }
-
-  // Метод append(str) - получает парметр str (строку) и добавляет ее в конец _value
-
   append(str) {
     this._value = this._value.concat(str);
     return this._value.concat(str);
   }
-
   prepend(str) {
     this._value = str.concat(this._value);
     return this._value;
@@ -149,13 +132,75 @@ class StringBuilder {
 }
 
 const builder = new StringBuilder(".");
-console.log(builder.value);
+console.log("#4 :", builder.value);
 
 builder.append("^");
-console.log(builder.value);
+console.log("#4 :", builder.value);
 
 builder.prepend("^");
-console.log(builder.value); // '^.^'
+console.log("#4 :", builder.value); // '^.^'
 
 builder.pad("=");
-console.log(builder.value); // '=^.^='
+console.log("#4 :", builder.value); // '=^.^='
+console.log("#============================================");
+// ===============================================================
+// Задание 5
+// ===============================================================
+class Car {
+  static getSpecs(object) {
+    console.log(
+      "#5 :",
+      `MaxSpeed : ${object.maxSpeed} km/h, `,
+      `Speed: ${object.speed} km/h,  `,
+      `isOn: ${object.isOn}, `,
+      `Distance: ${object.distance} km,  `,
+      `Price: ${object.price} $ `
+    );
+  }
+  constructor(object) {
+    (this.maxSpeed = object.maxSpeed),
+      object.speed ? (this.speed = object.speed) : (this.speed = 0),
+      (this.isOn = false),
+      object.distance ? (this.distance = object.distance) : (this.distance = 0),
+      (this._price = object.price);
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    return (this.isOn = true);
+  }
+  turnOff() {
+    return (this.isOn = false), (this.speed = 0);
+  }
+  decelerate(value) {
+    if (this.speed - value > 0) return (this.speed = this.speed - value);
+  }
+  accelerate(value) {
+    if (this.maxSpeed > this.speed + value)
+      return (this.speed = this.speed + value);
+  }
+  drive(hours) {
+    if (this.isOn) return (this.distance = this.distance + this.speed * hours);
+  }
+}
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+Car.getSpecs(mustang);
+console.log("#5 :", mustang.turnOn(), mustang.accelerate(50), mustang.drive(2));
+Car.getSpecs(mustang);
+console.log(
+  "#5 :",
+  mustang.decelerate(20),
+  mustang.drive(1),
+  mustang.turnOff()
+);
+Car.getSpecs(mustang);
+console.log("#5 : Price=", mustang.price); // 2000
+mustang.price = 4000;
+console.log("#5 :", mustang.price); // 4000
